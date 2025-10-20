f2fb6ae2980bc13ecac9ced85a102743740c7917
Rodolfo Ferreira <rodolfo.ferreira@trmlabs.com>
2025-10-20 14:26:22 -0300

    nvm_init

--- init.d/nvm.sh
+++ init.d/nvm.sh
@@ -1,3 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+function nvm_init {
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
